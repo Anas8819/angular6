61dd12fe214bbc49dfede2a59df905c786f7825b
Anas8819 <anasahmed8819@hotmail.com>
2019-02-14 22:38:16 +0500

    upto 10

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
@@ -9,17 +9,24 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class CreateEmployeeComponent implements OnInit {
 
   employeeForm: FormGroup;
-  constructor() { }
 
-  ngOnInit() {
-    this.employeeForm = new FormGroup({
-      fullName: new FormControl(),
-      email: new FormControl()
-    })
+  constructor(private fb: FormBuilder) {
   }
 
-  onSubmit(): void{
-    console.log(this.employeeForm.value);
+  ngOnInit() {
+    this.employeeForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      email: [''],
+      skills: this.fb.group({
+        skillName: [''],
+        experienceInYears: [''],
+        proficiency: ['']
+      })
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.employeeForm);
   }
 
 }
