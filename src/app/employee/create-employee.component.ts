5abf498b06ee2b85a7d5d472a2b51117251a5783
anas8819 <anasahmed8819@hotmail.com>
2019-02-15 17:38:51 +0500

    upto 14

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -8,7 +8,39 @@ import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
 
+  formErrors = {
+    fullName: '',
+    email: '',
+    phone: '',
+    skillName: '',
+    experienceInYears: '',
+    proficiency: ''
+  };
+
   employeeForm: FormGroup;
+
+  validationMessages = {
+    fullName: {
+      required: 'Full Name is required.',
+      minlength: 'Full Name must be greater than 2 characters.',
+      maxlength: 'Full Name must be less than 10 characters.'
+    },
+    email: {
+      required: 'Email is required.'
+    },
+    phone: {
+      required: 'Phone is required.'
+    },
+    skillName: {
+      required: 'Skill Name is required.',
+    },
+    experienceInYears: {
+      required: 'Experience is required.',
+    },
+    proficiency: {
+      required: 'Proficiency is required.',
+    },
+  };
 
   constructor(private fb: FormBuilder) {
   }
@@ -16,14 +48,60 @@ export class CreateEmployeeComponent implements OnInit {
   ngOnInit() {
     this.employeeForm = this.fb.group({
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
-      email: [''],
+      contactPreference: ['email'],
+      email: ['', Validators.required],
+      phone: [''],
       skills: this.fb.group({
-        skillName: [''],
-        experienceInYears: [''],
-        proficiency: ['']
+        skillName: ['', Validators.required],
+        experienceInYears: ['', Validators.required],
+        proficiency: ['', Validators.required]
       })
     });
+
+    this.employeeForm.controls.contactPreference.valueChanges.subscribe((data: string) => {
+      this.onContactPreferenceChange(data);
+    });
+
+    this.employeeForm.valueChanges.subscribe(value => {
+      this.logValidationErrors(this.employeeForm);
+    });
   }
+
+  onContactPreferenceChange(selectedValue: string) {
+    const phoneControl = this.employeeForm.controls.phone;
+    if (selectedValue === 'phone') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+  }
+
+  onLoadDataClick(): void {
+    this.logValidationErrors(this.employeeForm);
+    console.log(this.formErrors);
+  }
+
+  logValidationErrors(group: FormGroup = this.employeeForm): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractControl = group.get(key);
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
+      } else {
+        this.formErrors[key] = '';
+        if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)) {
+          const messages = this.validationMessages[key];
+          // console.log(messages);
+          for (const errorKey in abstractControl.errors) {
+            if (errorKey) {
+              this.formErrors[key] += messages[errorKey] + ' ';
+            }
+          }
+        }
+      }
+    });
+  }
+
 
   onSubmit(): void {
     console.log(this.employeeForm);
